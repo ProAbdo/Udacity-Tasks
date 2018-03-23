78e54e3c2d2df7e56ce8b06f02c1c59e299ed4b6
Abdurrahman Hussein <abdurrahmanhussein500@gmail.com>
2018-03-23 13:21:50 +0200

    Add Class

--- atm_solution/atm.py
+++ atm_solution/atm.py
@@ -1,18 +1,53 @@
-def atm(request):
-    while request>0:
-        while request>=100:
-            print 100
-            request-=100
-        while request>=50:
-            print 50
-            request-=50
-        while request>=10:
-            print 10
-            request-=10
-        while request>=5:
-            print 5
-            request-=5
-        if request>0:
-            print request
-        request-=request
-atm()
+class ATM:
+    def __init__(self,balance,bank_name):
+        self.balance=balance
+        self.bank_name=bank_name
+    def withdraw(self, request):
+        print "Welcome to "+ self.bank_name
+        print "Current balance = " + str(self.balance)
+        print "="*30
+        result = self.balance
+
+        if request > self.balance:
+            print("Can't give you all this money !!")
+        elif request < 0:
+            print("More than zero plz!")
+        else:
+            self.balance -= request
+            while request > 0:
+
+                if request >= 100:
+                    request -= 100
+                    print("give 100")
+
+                elif request >= 50:
+                    request -= 50
+                    print("give 50")
+
+                elif request >= 10:
+                    request -= 10
+                    print("give 10")
+
+                elif request >= 5:
+                    request -= 5
+                    print("give 5")
+
+                elif request < 5:
+                    print("give " + str(request))
+                    request = 0
+        print "="*30
+        return result    
+        print "="*30
+ 
+
+balance1 = 500
+balance2 = 1000
+
+atm1 = ATM(balance1, "Smart Bank")
+atm2= ATM(balance2, "Baraka Bank")
+
+atm1.withdraw(277)
+atm1.withdraw(800)
+
+atm2.withdraw(100)
+atm2.withdraw(2000)
